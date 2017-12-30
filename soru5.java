6f5f59980205c401f3b3335b2debbe79c8e355c3
Ahmet AL <34831119+ahmetall@users.noreply.github.com>
2017-12-30 14:38:17 +0300

    Create soru5.java

---
+++ soru5.java
@@ -0,0 +1,18 @@
+package finalcalisma;
+import java.util.Scanner;
+public class soru5 {
+
+	public static void main(String[] args) {
+		System.out.println("İfade Giriniz");
+		Scanner scan=new Scanner(System.in);
+		String ifade=scan.nextLine();
+		System.out.println("İfade Giriniz");
+		Scanner kackez=new Scanner(System.in);
+		int adet=kackez.nextInt();
+		for(int i=0;i<adet;i++)
+		{
+			System.out.println(ifade);
+		}
+	}
+
+}
