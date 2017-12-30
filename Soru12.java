c0d0ed1563486a91b01c1170c85c0d85ba28db50
Ahmet AL <34831119+ahmetall@users.noreply.github.com>
2017-12-30 15:50:59 +0300

    Create Soru12.java

---
+++ Soru12.java
@@ -0,0 +1,25 @@
+package finalcalisma;
+
+public class soru12 {
+
+	public static void main(String[] args) {
+		int x=6,y=6,z=8;
+		
+		if(x>y)
+		{
+			if(x>z)
+			System.out.println(x);
+			else
+			System.out.println(z);
+			}
+			else{
+			if(y>z)
+			System.out.println(y);
+			else
+			System.out.println(z);
+			}
+			}
+			
+	}
+
+
