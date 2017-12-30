de6df144edf468235c1cfed4f8641196e21c98a0
Ahmet AL <34831119+ahmetall@users.noreply.github.com>
2017-12-30 19:50:32 +0300

    Create Soru30.java

---
+++ Soru30.java
@@ -0,0 +1,14 @@
+package finalcalisma;
+
+import java.util.Scanner;
+public class soru30 {
+
+	public static void main(String[] args) {
+		System.out.println("Cümleyi Giriniz");
+		Scanner cumle=new Scanner(System.in);
+		String girilencumle=cumle.nextLine();
+		System.out.println("Harf Sayısı="+girilencumle.length());
+
+	}
+
+}
